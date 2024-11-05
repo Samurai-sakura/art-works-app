9e4cd567d4ce1fb30a422235b5cc3cac50ef7116
Aleksander <sokhoron@gmail.com>
2024-11-05 17:20:15 +0300

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-YVX36BHA.mjs";import"./chunk-XSVDBO6X.mjs";import"./chunk-FIQ4B6TP.mjs";import"./chunk-INGTML2I.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
